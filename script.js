5cb685ba9ab104aa149445391b19e6303945699c
Jesus Moreno <jmoreno91@alumnos.uaq.mx>
2023-05-05 11:50:50 -0600

    Se agrega el proyecto

---
+++ script.js
@@ -0,0 +1,73 @@
+MostrarCajitas();
+boton=document.getElementById("boton");
+boton.addEventListener("click",function(){
+    textoNuevo=document.getElementById("texto");
+    notas=localStorage.getItem("notas");
+    if(notas==null)
+    {
+        //No se ha agregado ninguna nota
+        arreglo=[];
+    }
+    else
+    {
+        arreglo=JSON.parse(notas);
+    }
+
+    arreglo.push(textoNuevo.value);
+    console.log(arreglo);
+    localStorage.setItem("notas",JSON.stringify(arreglo));
+   
+    textoNuevo.value="";
+    MostrarCajitas();
+});
+
+function MostrarCajitas()
+{
+    notas=localStorage.getItem("notas");
+    if(notas==null)
+    {
+        //No se ha agregado ninguna nota
+        arreglo=[];
+    }
+    else
+    {
+        arreglo=JSON.parse(notas);
+    }
+    html="";
+    arreglo.forEach((element,i) =>{
+        html=html+
+        `
+        <div>
+        <h3>Nota ${i}</h3>
+        <p>${element}</p>
+        <button id=${i} onclick="eliminar(this.id)">Eliminar</button>
+        </div>
+        `; 
+    });
+
+    contenedor=document.getElementById("misnotas");
+    contenedor.innerHTML=html;
+}
+function eliminar(index)
+{
+    notas=localStorage.getItem("notas");
+    if(notas==null)
+    {
+        //No se ha agregado ninguna nota
+        arreglo=[];
+    }
+    else
+    {
+        arreglo=JSON.parse(notas);
+    }
+    arreglo.splice(index,1);
+    localStorage.setItem("notas",JSON.stringify(arreglo));
+    MostrarCajitas();
+}
+
+function eliminarTodas()
+{
+    arreglo=[];
+    localStorage.setItem("notas",JSON.stringify(arreglo));
+    MostrarCajitas();
+}
